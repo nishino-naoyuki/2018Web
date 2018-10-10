0f40d92e6f7b92885d67520570b1a5200e781cf4
nishino-naoyuki <nishino@asojuku.ac.jp>
2018-10-10 09:48:33 +0900

    履歴テーブル

---
+++ 資料/rireki_tbl.sql
@@ -0,0 +1,9 @@
+CREATE TABLE webtestdb.rireki_tbl
+(
+	id int NOT NULL AUTO_INCREMENT,
+	img varchar(200) NOT NULL ,
+	reality int NOT NULL  ,
+	insertdate datetime NOT NULL,
+	mail varchar(256) NOT NULL,
+	PRIMARY KEY (id)
+);
